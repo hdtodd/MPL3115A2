5ccc24de8bf7ac875fecee698360c482587a1d4c
H. David Todd <hdtodd@gmail.com>
2015-08-13 09:46:56 -0600

    Updated example program with looping over all 8 Oversample rates

--- examples/Test_MPL3115A2_I2C/Test_MPL3115A2_I2C.ino
+++ examples/Test_MPL3115A2_I2C/Test_MPL3115A2_I2C.ino
@@ -1,5 +1,5 @@
 /*
-  Test_MPL3115A2.ino
+  Test_MPL3115A2-I2C.ino
   This program for the Arduino (Uno) tests the library of procedures for
   collecting readings from the Freescale MPL3115A2 altitude/barometric pressure/temperature
   sensor.
@@ -7,9 +7,9 @@
   This library uses Truchsess's I2C library for communication with the MPL3115A2 to avoid
   the misaligned data bits caused by repeated-starts with the older Wire library.
 
-  Sampling found to be unreliable over time when using the Wire library, <wire.h>
+  Sampling when using the Wire library, <wire.h>, was found to be unreliable over time
   Converted to use I2C library since it handles the repeated START I2C commands that the 
-  Freescale, Inc., devices use.
+  Freescale, Inc., devices appear to use
 
   Thanks to Wayne Truchsess for his excellent work on that system.
   See http://dsscircuits.com/articles/86-articles/66-arduino-i2c-master-library
@@ -23,16 +23,17 @@
     -VCC = 3.3V
     -SDA = A4		Add 330 ohm resistor in series if using 5v VCC
     -SCL = A5		Add 330 ohm resistor in series if using 5v VCC
+    -GND = GND
     -INT pins can be left unconnected for this demo
  
-    Usage:
-      -Serial terminal at 9600bps
-      -Times various measurements
-      -Examines status flags uses to poll device for data ready
+  Usage:
+    -Serial terminal at 9600bps
+    -Times various sensor measurements
+    -Monitors status flags used to poll device for data ready
  
   During testing, GPS with 9 satellites reported 5393ft, sensor reported 5360ft 
     (delta of 33ft). Very close! (Seidle)
-  During testing, GPS with 9 satellites reported 1520m; sensor reported 1433m; 
+  During testing in Bozeman, GPS with 9 satellites reported 1520m; sensor reported 1433m; 
     surprisingly far off (Todd)
     [So I chose to tell it what the altitude is and have it apply the correction -- Todd]
  */
@@ -41,6 +42,7 @@
 #include "MPL3115A2.h"
 
 #define MY_ALTITUDE 1520           // Set this to your actual GPS-verified altitude in meters if needed
+                                   //   or set to 0 for no correction
 
 uint8_t sampleRate=S_1;            // We'll cycle through all 8 settings, but start with 1 sample/reading
 MPL3115A2 thing;                   // This is the thing we'll be sampling
@@ -59,8 +61,11 @@ void setup()
     exit(1);
   }
 
-  thing.setAltitude(MY_ALTITUDE);       // try with MY_ALTITUDE set to 0 and see if altimeter is
-                                        // accurate; if not, set MY_ALTITUDE to GPS-provided altitude in meters
+// ALTITUDE CORRECTION (IF DESIRED)  
+// try with MY_ALTITUDE set to 0 and see if altimeter is
+// accurate; if not, set MY_ALTITUDE to GPS-provided altitude in meters
+  if (MY_ALTITUDE != 0) thing.setAltitude(MY_ALTITUDE);
+
 
 // And announce ourselves
   Serial.println("Characterizing MPL3115A2 performance on an Arudino with access via I2C library"); 
